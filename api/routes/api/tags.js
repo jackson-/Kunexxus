179793dad2fffce1c5fe3b06f11f1880295d17e3
jackson- <jackson.t.devin@gmail.com>
2017-09-16 23:11:42 -0400

    fresh start

--- api/routes/api/tags.js
+++
@@ -1,9 +0,0 @@
-var router = require('express').Router();
-var mongoose = require('mongoose');
-
-// return a list of tags
-router.get('/', function(req, res, next) {
-    return res.json({tags: []});
-});
-
-module.exports = router;
--- web/src/reducers/common.js
+++
@@ -1,7 +0,0 @@
-const defaultState = {
-  appName: 'Kunexxus'
-};
-
-export default (state = defaultState, action) => {
-  return state;
-};
